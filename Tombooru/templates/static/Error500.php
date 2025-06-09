583ee3ee988d7bb44281055f1d0af47a7bef8c76
Michiel Sikma <michiel@sikma.org>
2025-06-09 15:08:51 +0200

    Add error message if admin

--- Tombooru/templates/static/Error500.php
+++ Tombooru/templates/static/Error500.php
@@ -1,4 +1,7 @@
 <div class="tombooru-page">
   <h1>Internal error</h1>
   <p>Sorry. Something went wrong and I have no idea what.</p>
+  <?php if (@$request['user']['isAdmin']): ?>
+    <pre><?= htmlentities($error); ?></pre>
+  <?php endif; ?>
 </div>
